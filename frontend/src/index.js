fcdeae18f8ae46638043631f02e4d5147b8cf775
Thanh Phong Le <ltphongssvn@gmail.com>
2025-11-10 07:48:12 -0800

    feat: Add React index.js entry point

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+// ~/code/ltphongssvn/Supply-Chain-Optimization-Platform/frontend/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
